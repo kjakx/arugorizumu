104a407e642ab65a38b4ae0eef198fc619c4a436
kjakx <fxhfr163@ybb.ne.jp>
2020-03-26 11:47:50 +0000

    some comments

--- ch05/kmp_matching.cpp
+++ ch05/kmp_matching.cpp
@@ -18,14 +18,21 @@ int main()
     else cout << "pattern found at " << pos << endl;
 }
 
+// kmp matching is a pattern matching method
+// that uses failure function which tells
+// what to do next when the matching failed.
+// time complexity: O(n+m) where n is the length of a text and m is the length of a pattern
 int kmp_matching(string& text, string& pattern, vector<int>& f)
 {
     failure_function(pattern, f);
     int i_pattern = 0;
     int i_head = 0;
     int pos = -1;
+    // compared unsigned int, negative int is casted to corresponding int.
+    // to prevent it, size() is casted to int before it is compared with negative int.
     while (i_pattern <= int(pattern.size() - 1) && i_head <= int(text.size() - 1))
     {
+        // i_pattern -1 means that the matching between the first character of pattern and text was failed.
         if (i_pattern == -1 || pattern[i_pattern] == text[i_head])
         {
             i_pattern++;
@@ -33,6 +40,8 @@ int kmp_matching(string& text, string& pattern, vector<int>& f)
         }
         else
         {
+            // f[i] tells which charater in pattern to be compared to text[i_head] next
+            // when comparison between text[i_head] and pattern[i] was failed. 
             i_pattern = f[i_pattern];
         }
     }
@@ -48,8 +57,10 @@ void failure_function(string& pattern, vector<int>& f)
     int i_pattern = -1;
     int i_head = 0;
     f[0] = -1;
+    // comparing the partial pattern which begins at i_head with the pattern itself.
     while(i_head < pattern.size() - 1)
     {
+        // i_pattern -1 means comparison of the first character was failed.
         if (i_pattern == -1 || pattern[i_pattern] == pattern[i_head])
         {
             i_pattern++;
