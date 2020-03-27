6d16bc84fc5da1d0cd60de49e5457ef195a40c4e
kjakx <fxhfr163@ybb.ne.jp>
2020-03-27 08:50:53 +0000

    bm matching but core dump occured

--- ch05/bm_matching.cpp
+++ ch05/bm_matching.cpp
@@ -5,15 +5,23 @@
 
 using namespace std;
 
-void g(string& pattern, vector<int>& g);
-void h(string& pattern, vector<int>& h, vector<int>& f);
+void g_function(string& pattern, vector<int>& g);
+void h_function(string& pattern, vector<int>& h, vector<int>& f);
+int bm_matching(string& text, string& pattern, vector<int>& f, vector<int>& g, vector<int>& h);
 
 int main()
 {
+    string text = "ababdababccbdcabcadb";
+    string pattern = "ababc";
+    vector<int> f(pattern.size()), g(256), h(pattern.size());
+    int pos;
+    pos = bm_matching(text, pattern, f, g, h);
+    if (pos < 0) cout << "not found" << endl;
+    else cout << "pattern found at " << pos << endl;
     return 0;
 }
 
-void g(string& pattern, vector<int>& g)
+void g_function(string& pattern, vector<int>& g)
 {
     for (int c = 0; c <= 255; c++)
     {
@@ -21,16 +29,16 @@ void g(string& pattern, vector<int>& g)
     }
     for (int k = 0; k < pattern.size(); k++)
     {
-        g[pattern[k]] = pattern.size() - 1 - k;
+        g[pattern[k]] = int(pattern.size()) - 1 - k;
     }
 }
 
-void h(string& pattern, vector<int>& h, vector<int>& f)
+void h_function(string& pattern, vector<int>& h, vector<int>& f)
 {
     int i, j;
     for (i = 0; i < pattern.size(); i++)
     {
-        h[i] = 2 * (pattern.size() - 1) - 1;
+        h[i] = 2 * (pattern.size() - 1) - i;
     }
     i = pattern.size() - 1;
     j = pattern.size();
@@ -51,10 +59,34 @@ void h(string& pattern, vector<int>& h, vector<int>& f)
     }
     for (i = 0; i < pattern.size(); i++)
     {
-        h[i] = min(h[j], int(pattern.size()) - j - 1);
+        h[i] = min(h[j], int(pattern.size()) - 1 + j - i);
         if (i >= j)
         {
             j = f[j];
         }
     }
+}
+
+int bm_matching(string& text, string& pattern, vector<int>& f, vector<int>& g, vector<int>& h)
+{
+    g_function(pattern, g);
+    h_function(pattern, h, f);
+    int i = pattern.size() - 1;
+    int j = pattern.size() - 1;
+    int pos = -1;
+    while (i >= 0 && j < int(text.size()))
+    {
+        if (pattern[i] == text[j])
+        {
+            i--;
+            j--;
+        }
+        else
+        {
+            j = j + max(g[text[j]], h[i]);
+            i = pattern.size() - 1;
+        }
+    }
+    if (i < 0) pos = j + 1;
+    return pos;
 }
